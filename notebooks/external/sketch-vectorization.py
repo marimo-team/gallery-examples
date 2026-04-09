207d3a48dc616a5f0ca9244a4ab14e4f6734ce78
Akshay Agrawal <akshay@marimo.io>
2026-04-09 12:38:02 -0700

    Update script metadata

--- notebooks/external/sketch-vectorization.py
+++ notebooks/external/sketch-vectorization.py
@@ -1,12 +1,9 @@
 # /// script
 # requires-python = ">=3.12"
 # dependencies = [
-#     "marimo",
 #     "sketchy-svg",
-#     "yarl",
-#     "scipy",
-#     "scikit-image",
-#     "onnxruntime ; sys_platform != 'emscripten'",
+#     "marimo",
+#     "yarl"
 # ]
 # ///
 
